87d73ef0d3cfcf7079555d001d45f63aee1c61f3
Chan <chan@vietswiss.com>
2022-07-14 15:42:25 +0700

    feat: init tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
+  <div class="text-center bg-sky-400 h-[80px] flex items-center justify-center text-white">
+    <h1 class="text-[24px] font-black">BC MANAGER</h1>
+  </div>
+  <div class="container mx-auto">
     <!-- This page correctly has only one single root element -->
     Page content
   </div>
